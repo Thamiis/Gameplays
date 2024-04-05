faf96e68ca1eb864cd047e0390a4b8bbe7231b50
Gustavo <gustavoocs1@gmail.com>
2024-04-04 23:41:28 -0300

    title styles and div tests

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ function App() {
 
   return (
     <>
-    <h1>Gameplays</h1>
+    <button className='hvr-pulse'>Gameplays</button>
+    <div className='Home'>
+    <div className='JogoHome1'>Jogo1</div>
+    <div className='JogoHome2'>Jogo2</div>
+    <div className='JogoHome3'>Jogo3</div>
+    </div>
+
+   
+    
+    
     </>
   )
 }
